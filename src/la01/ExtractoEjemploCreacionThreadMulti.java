305fd115123a6041e5b409b1419279363656433c
AdrianMasia <61563754+AdrianMasia@users.noreply.github.com>
2022-10-04 09:54:41 +0200

    Laboratorio 1 hasta ejercicio 5.6 inclusive

--- src/la01/ExtractoEjemploCreacionThreadMulti.java
+++ src/la01/ExtractoEjemploCreacionThreadMulti.java
@@ -1,33 +1,43 @@
-/*
 package la01;  // --------------------------------------------------------------------------
-  public static void main( String args[] ) {
-    int  numHebras;
+class EjemploCreacionThreadMulti {
+    public static void main(String args[]) {
+        int numHebras;
 
-    // Comprobacion y extraccion de los argumentos de entrada.
-    if( args.length != 1 ) {
-      System.err.println( "Uso: java programa <numHebras>" );
-      System.exit( -1 );
+        // Comprobacion y extraccion de los argumentos de entrada.
+        if (args.length != 1) {
+            System.err.println("Uso: java programa <numHebras>");
+            System.exit(-1);
+        }
+        try {
+            numHebras = Integer.parseInt(args[0]);
+        } catch (NumberFormatException ex) {
+            numHebras = -1;
+            System.out.println("ERROR: Argumentos numericos incorrectos.");
+            System.exit(-1);
+        }
+
+        System.out.println("numHebras: " + numHebras);
+
+        // --------  CODIGO PRINCIPAL TRAS PROCESAR PARAMETROS  -------------------
+
+        System.out.println("Hebra Principal inicia");
+        // Crear un vector (v) de tipo MiHebra con numHebras elementos
+        MiHebra v[] = new MiHebra[numHebras];
+        // Crear y arrancar las hebras y almacenarlas en v
+        for (int i = 0; i < numHebras; i++) {
+            MiHebra t = new MiHebra(i, 1, 1000000);
+            v[i] = t;
+            t.start();
+        }
+        // Esperar a que terminen todas las hebras almacenadas en v
+        try {
+            for (int i = 0; i < numHebras; i++) {
+                v[i].join();
+            }
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        System.out.println("Hebra Principal finaliza");
     }
-    try {
-      numHebras = Integer.parseInt( args[ 0 ] );
-    } catch( NumberFormatException ex ) {
-      numHebras = -1;
-      System.out.println( "ERROR: Argumentos numericos incorrectos." );
-      System.exit( -1 );
-    }
-
-    System.out.println( "numHebras: " + numHebras );
-   
-    // --------  CODIGO PRINCIPAL TRAS PROCESAR PARAMETROS  -------------------
-
-    System.out.println( "Hebra Principal inicia" );
-    // Crear un vector (v) de tipo MiHebra con numHebras elementos
-    MiHebra v[] = new MiHebra[ numHebras ];
-    // Crear y arrancar las hebras y almacenarlas en v
-    // ...
-    // Esperar a que terminen todas las hebras almacenadas en v
-    // ...
-    System.out.println( "Hebra Principal finaliza" );
-  }
-  // --------------------------------------------------------------------------
-*/
+    // --------------------------------------------------------------------------
+}
--- src/la01/ExtractoEjemploCreacionThreadSumas.java
+++ src/la01/ExtractoEjemploCreacionThreadSumas.java
@@ -20,15 +20,54 @@ class MiHebra extends Thread {
 
 class EjemploCreacionThreadSumas {
   public static void main( String args[] ) {
+/*    int numHebras;
+
+    // Comprobacion y extraccion de los argumentos de entrada.
+    if(args.length != 1) {
+      System.err.println("Uso: java programa <numHebras>");
+      System.exit(-1);
+    }
+    try{
+      numHebras = Integer.parseInt(args[0]);
+    }catch (NumberFormatException ex){
+      numHebras = -1;
+      System.out.println("ERROR: Argumentos numericos incorrectos.");
+      System.exit(-1);
+    }
+
+    System.out.println("numHebras: " + numHebras);
+
+    //-------CODIGO PRINCIPAL TRAS PROCESAR PARAMETROS-------*/
+
     System.out.println( "Hebra Principal inicia" );
+
     // Crea y arranca hebra t0 sumando desde 1 hasta 1000000
     MiHebra t0 = new MiHebra(0, 1, 1000000);
-    t0.setDaemon(true);
+//    t0.setDaemon(true);
     t0.start();
     // Crea y arranca hebra t1 sumando desde 1 hasta 1000000
     MiHebra t1 = new MiHebra(1, 1, 1000000);
-    t1.setDaemon(true);
+//    t1.setDaemon(true);
     t1.start();
+    try {
+      t0.join();
+      t1.join();
+    } catch (InterruptedException ex) {ex.printStackTrace();}
+
+/*    // Crear un vector (v) de tipo MiHebra con numHebras elementos
+    MiHebra[] v = new MiHebra[numHebras];
+    // Crear y arrancar las hebras y almacenarlas en v
+    for(int i = 0; i < numHebras; i++) {
+      MiHebra t = new MiHebra(i, 1, 1000000);
+      v[i] = t;
+      t.start();
+    }
+    // Esperar a que terminen todas las hebras almacenadas en v
+    try{
+      for(int i = 0; i < numHebras; i++){
+        v[i].join();
+      }
+    }catch (InterruptedException ex){ex.printStackTrace();}*/
     System.out.println( "Hebra Principal finaliza" );
   }
 }
