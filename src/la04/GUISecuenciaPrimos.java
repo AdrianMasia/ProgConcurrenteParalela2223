50a662a776ad35ba15ad9fc587bc460933fe72e5
AdrianMasia <61563754+AdrianMasia@users.noreply.github.com>
2022-10-27 00:14:47 +0200

    Laboratorio 4 acabado

--- src/la04/GUISecuenciaPrimos.java
+++ src/la04/GUISecuenciaPrimos.java
@@ -2,26 +2,27 @@ package la04;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
 import javax.swing.*;
 import javax.swing.event.*;
 
-/*
+
 // ===========================================================================
-class ZonaIntercambio1a {
+class ZonaIntercambio {
 // ===========================================================================
-  // ...
+  AtomicLong tiempo = new AtomicLong(500);
 
   // -------------------------------------------------------------------------
-  void setTiempo( ... ) {
-    // ...
+  void setTiempo( long tiempoMilisegundos ) {
+    tiempo.set(tiempoMilisegundos);
   }
 
   // -------------------------------------------------------------------------
-  long getTiempo( ... ) {
-    // ...
+  long getTiempo( ) {
+    return tiempo.get();
   }
 }
-*/
 
 // ===========================================================================
 public class GUISecuenciaPrimos {
@@ -31,8 +32,8 @@ public class GUISecuenciaPrimos {
   JTextField  txfMensajes;
   JButton     btnComienzaSecuencia, btnCancelaSecuencia;
   JSlider     sldEspera;
-//  HebraCalculadora1a  t; // Ejercicio 2.2
-//  ZonaIntercambio1a   z; // Ejercicio 2.3
+  HebraCalculadora  t; // Ejercicio 2.2
+  ZonaIntercambio   z = new ZonaIntercambio(); // Ejercicio 2.3
   
   // -------------------------------------------------------------------------
   public static void main( String args[] ) {
@@ -54,7 +55,7 @@ public class GUISecuenciaPrimos {
     JPanel  tempPanel;
 
     // Crea el JFrame principal.
-    container = new JFrame( "GUI Secuencia de Primos 1a" );
+    container = new JFrame( "GUI Secuencia de Primos " );
 
     // Consigue el panel principal del Frame "container".
     jpanel = ( JPanel ) container.getContentPane();
@@ -88,19 +89,24 @@ public class GUISecuenciaPrimos {
     
     // Activa inicialmente los 2 botones.
     btnComienzaSecuencia.setEnabled( true );
-    btnCancelaSecuencia.setEnabled( true );
+    btnCancelaSecuencia.setEnabled( false );
 
     // Anyade codigo para procesar el evento del boton de Comienza secuencia.
     btnComienzaSecuencia.addActionListener( new ActionListener() {
         public void actionPerformed( ActionEvent e ) {
-          // ...
+          btnComienzaSecuencia.setEnabled( false );
+          btnCancelaSecuencia.setEnabled( true );
+          t = new HebraCalculadora(txfMensajes, z);
+          t.start();
         }
     } );
 
     // Anyade codigo para procesar el evento del boton de Cancela secuencia.
     btnCancelaSecuencia.addActionListener( new ActionListener() {
         public void actionPerformed( ActionEvent e ) {
-          // ...
+          btnComienzaSecuencia.setEnabled( true );
+          btnCancelaSecuencia.setEnabled( false );
+          t.fin();
         }
     } );
 
@@ -111,7 +117,7 @@ public class GUISecuenciaPrimos {
         if ( ! sl.getValueIsAdjusting() ) {
           long tiempoMilisegundos = ( long ) sl.getValue();
           System.out.println( "JSlider value = " + tiempoMilisegundos );
-          // ...
+          z.setTiempo(tiempoMilisegundos);
         }
       }
     } );
@@ -141,3 +147,45 @@ public class GUISecuenciaPrimos {
     return( primo );
   }
 }
+
+class HebraCalculadora extends Thread {
+  AtomicBoolean fin = new AtomicBoolean();
+  JTextField txfMensajes;
+  ZonaIntercambio z;
+  AtomicLong tiempo = new AtomicLong();
+
+  public HebraCalculadora(JTextField txfMensajes, ZonaIntercambio z) {
+    this.fin.set(false);
+    this.txfMensajes = txfMensajes;
+    this.z = z;
+  }
+
+  public void fin() {
+    fin.set(true);
+  }
+
+  @Override
+  public void run() {
+    long i = 1L;
+    while (!fin.get()) {
+      if(GUISecuenciaPrimos.esPrimo(i)) {
+        long primo = i;
+        tiempo.set(z.getTiempo());
+        try {
+          sleep(tiempo.get());
+        } catch (InterruptedException e) {
+          throw new RuntimeException(e);
+        }
+        SwingUtilities.invokeLater(new Runnable() {
+          @Override
+          public void run() {
+            String mensaje = Long.toString(primo);
+            txfMensajes.setText(mensaje);
+          }
+        });
+      }
+      i++;
+    }
+  }
+}
+
